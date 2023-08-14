61450c7e35061303350c5f1291ebe9ceae02ee1e
Md Habibur Rahman <39822204+Habib16051@users.noreply.github.com>
2023-08-14 10:54:42 +0600

    Update urls.py

--- Khoj_Project/urls.py
+++ Khoj_Project/urls.py
@@ -14,9 +14,9 @@ urlpatterns = [
     path('api/', include('api.urls')),
 
     # jwt
-	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-	path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
